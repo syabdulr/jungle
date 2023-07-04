4c56d1c67dac5399d2e3c0b2f02d9d8a03efa66c
@syabdulr <abdulsyed1998@gmail.com>
2023-07-04 14:02:36 -0400

    add testing for home page

---
+++ cypress/integration/home.spec.js
@@ -0,0 +1,13 @@
+describe('Home Page', () => {
+  it('successfully loads', () => {
+    cy.visit('/') // Visit the root URL
+  });
+
+  it("There are products on the page", () => {
+    cy.get(".products article").should("be.visible");
+  });
+
+  it("There are 2 products on the page", () => {
+    cy.get(".products article").should("have.length", 2);
+  });
+});
